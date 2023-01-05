d5189cac8d5bcfb0ca5101bda1cf33664a9dacc1
mankarkomal7747 <119654877+mankarkomal7747@users.noreply.github.com>
2023-01-05 15:41:18 +0530

    Add files via upload

---
+++ spring-core/src/main/java/com/jsp/Demo/Pen.java
@@ -0,0 +1,12 @@
+package com.jsp.Demo;
+
+public class Pen {
+
+	Pen(){
+		System.out.println("constuctor is loading");
+	}
+	public void write() {
+	System.out.println("pen is writing");
+		
+	}
+}
